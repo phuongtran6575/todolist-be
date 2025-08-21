4cd7989bd997f971083b2bcd3b533d29e3c15e43
keoma <keomacnha690@gmail.com>
2025-08-21 23:42:18 +0700

    fix 1 file

--- app/controller/todo_controller.py
+++ app/controller/todo_controller.py
@@ -4,24 +4,24 @@ from service import todo_service
 from database.sqlite_database import SessionDepends
 from models.todo_model import Todo
 
-router = APIRouter(prefix="/todo", tags=["Todo"])
+router = APIRouter(prefix="/todos", tags=["Todo"])
 
-@router.get("/todos")
+@router.get("/")
 async def get_all_todos(session: SessionDepends):
     return await todo_service.get_all_todos(session)
 
-@router.get("/todo/{todo_id}")
+@router.get("/{todo_id}")
 async def get_todo_by_id(todo_id:int,session: SessionDepends):
     return await todo_service.get_todo_by_id(todo_id, session)
 
-@router.post("/todo")
+@router.post("/")
 async def add_todo(todo: Todo, session: SessionDepends):
     return await todo_service.add_todo(todo, session)
 
-@router.put("/todo")
+@router.put("/")
 async def update_todo(todo: Todo, session: SessionDepends):
     return await todo_service.update_todo(todo, session)
 
-@router.delete("/todo/{todo_id}")
+@router.delete("/{todo_id}")
 async def delete_todo(todo_id:int, session: SessionDepends):
     return await todo_service.delete_todo (todo_id, session)

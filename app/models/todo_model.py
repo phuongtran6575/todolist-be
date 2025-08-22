2d4bc2ef0bf16f335576f7670b9e5f946d9b8fae
tranphuong670 <tranhongphuong272@gmail.com>
2025-08-22 16:08:12 +0700

    fix 1 file

--- app/models/todo_model.py
+++ app/models/todo_model.py
@@ -1,4 +1,9 @@
+import datetime
 from sqlmodel import SQLModel, Field
 class Todo(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
+    description: str
     name: str
+    created_ad:  datetime = Field(default_factory=datetime.utcnow)
+    due_at: datetime 
+    isDone: bool

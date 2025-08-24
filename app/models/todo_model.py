53e755f168d1d8b8d4fbf6063c45e979537b7e79
keoma <keomacnha690@gmail.com>
2025-08-24 23:00:21 +0700

    fix 2 file

--- app/models/todo_model.py
+++ app/models/todo_model.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from sqlmodel import SQLModel, Field
+from typing import Optional
 
 class Todo(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    description: str
-    name: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    due_at: datetime
-    isDone: bool
+    id: Optional[int] = Field(default=None, primary_key=True)
+    description: Optional[str]
+    name: Optional[str]
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    due_at: Optional[datetime]
+    isDone: Optional[bool]
--- app/service/todo_service.py
+++ app/service/todo_service.py
@@ -29,10 +29,16 @@ async def add_todo(todo: Todo, session: Session):
     return todo_data
 
 async def update_todo(todo: Todo, session: Session):
-    statement = select.select(Todo).where(Todo.id == todo.id)
+    statement = select(Todo).where(Todo.id == todo.id)
+    parse_due_at = datetime.fromisoformat(todo.due_at.replace("Z", "+00:00"))
     todo_to_update = session.exec(statement).first()
+    todo_to_update.description= todo.description
+    todo_to_update.isDone = todo.isDone
+    todo_to_update.due_at= parse_due_at
     todo_to_update.name = todo.name
     session.add(todo_to_update)
+    session.commit()
+    session.refresh(todo_to_update)
     return todo_to_update
 
 async def delete_todo(todo_id: int, session: Session):
@@ -40,4 +46,4 @@ async def delete_todo(todo_id: int, session: Session):
     todo = session.exec(statement).first()
     session.delete(todo)
     session.commit()
-    return {"delete"}
+    return {"status":"delete"}

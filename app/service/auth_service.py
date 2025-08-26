e6becb082e4e9dbabf532ea199df793901dc0689
keoma <keomacnha690@gmail.com>
2025-08-26 22:21:18 +0700

    fix 1 file

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -1,12 +1,22 @@
 from sqlmodel import Session, select
 from models.user_model import User
+import jwt
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_hash_password(password):
+    return pwd_context.hash(password)
 
 async def get_user_by_username(username: str, session: Session):
     statement = select(User).where(User.username == username)
     user_db = session.exec(statement).first()
     return user_db
-
-
 
 async def registered(user:User ,session:Session):
     session.add(user)

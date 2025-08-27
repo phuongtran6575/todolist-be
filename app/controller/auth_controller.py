8c1340bdd1bcdb4492d2664d689a6626790a568c
tranphuong670 <tranhongphuong272@gmail.com>
2025-08-27 10:50:50 +0700

    fix 3 file authentication

--- app/controller/auth_controller.py
+++ app/controller/auth_controller.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from core.constants import ACCESS_TOKEN_EXPIRE_MINUTES
 from service import auth_service
 from database.sqlite_database import SessionDepends
 from models.user_model import User 
@@ -9,16 +11,25 @@ from models.user_model import User
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/token")
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-@router.get("/")
-async def read_me(current_user= Annotated[str, Depends(oauth2_schema)]):
-    return current_user
+@router.get("/me")
+async def read_me(token: Annotated[str, Depends(oauth2_schema)]):
+    return {"token": token}
 
 @router.post("/token")
 async def login(session: SessionDepends,form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_db = await auth_service.get_user_by_username(form_data.username, session)
-    if user_db.password == form_data.password:
-        return {"access": user_db.username}
+    
+    if not user_db or not auth_service.verify_password(form_data.password, user_db.password):
+        return HTTPException(status_code=401, detail="Invalid credentials")  
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth_service.create_access_token(data = {"sub":str(user_db.id)}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/")
 async def register(user:User, session: SessionDepends):
     return await auth_service.registered(user, session)
+
+@router.delete("/{user_id}")
+async def delete_user(user_id: int, session:SessionDepends):
+    return await auth_service.delete(user_id, session)
--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta, timezone
 from sqlmodel import Session, select
+from core.constants import ALGORITHM, SECRET_KEY
 from models.user_model import User
 import jwt
 from jwt.exceptions import InvalidTokenError
@@ -6,6 +8,19 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token: str, session: Session):
+    
+    return
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -19,7 +34,17 @@ async def get_user_by_username(username: str, session: Session):
     return user_db
 
 async def registered(user:User ,session:Session):
-    session.add(user)
+    hashed_pw = get_hash_password(user.password)
+    user_db = User(username= user.username, password=hashed_pw)
+    session.add(user_db)
     session.commit()
-    session.refresh(user)
-    return user
+    session.refresh(user_db)
+    return user_db
+
+async def delete(user_id:int, session: Session):
+    statement = select(User).where(user_id == User.id)
+    user_db = session.exec(statement).first()
+    session.delete(user_db)
+    session.commit()
+    return {"type": "delete"}
+    

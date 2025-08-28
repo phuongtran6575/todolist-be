053c1f92a27541676ccd90991587ee80ac5bf0aa
tranphuong670 <tranhongphuong272@gmail.com>
2025-08-28 15:52:28 +0700

    deleteve version

--- app/models/user_model.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: Optional[str]
-    password: Optional[str]

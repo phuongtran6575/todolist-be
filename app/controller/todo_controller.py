c9a9bb5f9c5b19d84099003298916556ab34ca13
tranphuong670 <tranhongphuong272@gmail.com>
2025-08-29 09:52:21 +0700

    add authentication for todo, change todo_controller, todo_service

--- app/controller/todo_controller.py
+++ app/controller/todo_controller.py
@@ -1,36 +1,44 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from service import auth_service
 from service.todo_service import get_all_todos, add_todo, update_todo, delete_todo
 from service import todo_service
 from database.sqlite_database import SessionDepends
 from models.todo_model import Todo
 
 router = APIRouter(prefix="/todos", tags=["Todo"])
-
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/token")
 @router.get("/")
-async def get_all_todos(session: SessionDepends):
-    return await todo_service.get_all_todos(session)
+async def get_all_todos(token:Annotated[str, Depends(oauth2_schema)] ,session: SessionDepends ):
+    current_user = auth_service.get_current_user(token, session)
+    return await todo_service.get_all_todos(current_user.id, session)
 
 @router.get("/{todo_id}")
-async def get_todo_by_id(todo_id:int,session: SessionDepends):
-    todo = await todo_service.get_todo_by_id(todo_id, session)
+async def get_todo_by_id(token: Annotated[str, Depends(oauth2_schema)] ,todo_id:int, session: SessionDepends):
+    current_user = auth_service.get_current_user(token, session)
+    todo = await todo_service.get_todo_by_id(current_user.id ,todo_id, session)
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
 @router.post("/")
-async def add_todo(todo: Todo, session: SessionDepends):
-    return await todo_service.add_todo(todo, session)
+async def add_todo(token: Annotated[str, Depends(oauth2_schema)] ,todo: Todo, session: SessionDepends):
+    current_user = auth_service.get_current_user(token, session)
+    return await todo_service.add_todo(current_user.id ,todo, session)
 
 @router.put("/{todo_id}")
-async def update_todo(todo_id:int, todo: Todo, session: SessionDepends):
-    todo = await todo_service.update_todo(todo_id, todo, session)
+async def update_todo( token: Annotated[str, Depends(oauth2_schema)] ,todo_id:int, todo: Todo, session: SessionDepends):
+    current_user = auth_service.get_current_user(token, session)
+    todo = await todo_service.update_todo(current_user.id, todo_id, todo, session)
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
 @router.delete("/{todo_id}")
-async def delete_todo(todo_id:int, session: SessionDepends):
-    todo = await todo_service.delete_todo (todo_id, session)
+async def delete_todo( token: Annotated[str, Depends(oauth2_schema)] ,todo_id:int, session: SessionDepends):
+    current_user = auth_service.get_current_user(token, session)
+    todo = await todo_service.delete_todo (current_user.id, todo_id, session)
     if not todo:
         raise HTTPException(status_code=404, detail=" Todo not found")
     return todo
--- app/service/todo_service.py
+++ app/service/todo_service.py
@@ -2,38 +2,41 @@ from datetime import datetime
 from models.todo_model import Todo
 from sqlmodel import Session, select
 
-async def get_all_todos(session: Session):
-    statement = select(Todo)
+async def get_all_todos(user_id:int, session: Session, ):
+    statement = select(Todo).where(Todo.user_id == user_id)
     todos = session.exec(statement).all()
     return todos
 
-async def get_todo_by_id(todo_id: int, session: Session):
+async def get_todo_by_id( user_id: int, todo_id: int, session: Session,):
     statement = select(Todo).where(Todo.id == todo_id)
     todo = session.exec(statement).first()
-    if not todo:
+    if not todo or todo.user_id != user_id:
         return None
     return todo
 
-async def add_todo(todo: Todo, session: Session):
+async def add_todo(user_id:int, todo: Todo, session: Session, ):
     parse_due_at = datetime.fromisoformat(todo.due_at.replace("Z", "+00:00")) 
+    parse_created_at = datetime.fromisoformat(todo.created_at.replace("Z", "+00:00"))
     todo_data= Todo(
         id = todo.id,
         description=todo.description,
         isDone= todo.isDone,
         name=todo.name,
-        created_at=todo.created_at,
-        due_at=parse_due_at
+        #created_at=todo.created_at,
+        created_at=parse_created_at,
+        due_at=parse_due_at,
+        user_id=user_id
     )
     session.add(todo_data)
     session.commit()
     session.refresh(todo_data)
     return todo_data
 
-async def update_todo(todo_id: int, todo: Todo, session: Session):
+async def update_todo( user_id: int, todo_id: int, todo: Todo, session: Session,):
     statement = select(Todo).where(Todo.id == todo_id)
     todo_to_update = session.exec(statement).first()
-    if not todo_to_update:
-        return None   # hoặc raise HTTPException(404, "Todo not found")
+    if not todo_to_update or todo_to_update.user_id != user_id:
+        return None   
 
     # parse datetime
     parse_due_at = datetime.fromisoformat(todo.due_at.replace("Z", "+00:00"))
@@ -49,10 +52,10 @@ async def update_todo(todo_id: int, todo: Todo, session: Session):
     session.refresh(todo_to_update)
     return todo_to_update
 
-async def delete_todo(todo_id: int, session: Session):
+async def delete_todo(user_id:int, todo_id: int, session: Session, ):
     statement = select(Todo).where(Todo.id == todo_id)
     todo = session.exec(statement).first()
-    if not todo:
+    if not todo or todo.user_id != user_id:
         return None
     session.delete(todo)
     session.commit()
